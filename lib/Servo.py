972d41ca1522c31a37fd3820bb5289d2af88cd2b
Jon Hemstreet <jonhemstreet@gmail.com>
2017-01-04 04:15:09 +0000

    adding lib stubbed files

---
+++ lib/Servo.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO
+
+
+class Servo:
+    def __init__(self, config):
+        print("Initializing Servo")
+        pass
+
+    def to_position(self, servo, amount):
+        pass
